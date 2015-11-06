18b9432c2999ff2da4b39e2715513c3d237c72e3
xDinomode <xDinomode@users.noreply.github.com>
2015-11-05 18:13:17 -0600

    added express

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var http = require("http");
+var express = require("express");
+var app = express();
 
-http.createServer(function(req, res){
-	res.writeHead(200, {"Contet-Type" : "text/plain"});
-	res.write("Testing my github website");
-	res.end();
-}).listen(3000);
+app.get("/", function(req, res){
+	res.send("Hello world");
+});
+
+app.listen(3000);
