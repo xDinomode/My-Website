bc0f240a0be4d8f58b017b0ee2de66d219cbde08
xDinomode <xDinomode@users.noreply.github.com>
2015-11-07 11:21:19 -0600

    added purecss.io

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-	res.send("Hello world again");
-});
+var bodyParser = require("body-parser");
+
+var route = require("./routes/mainrouter");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//app.enable("trust proxy");
+//app.set("trust proxy", "loopback");
+
+//TODO: remove this and let nginx deliver static content later!!
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", route);
 
 app.listen(3000);
---
+++ routes/mainrouter.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var route = express.Router();
+
+route.get("/", function(req, res){
+	res.render("index", {title: "Home Page"});
+});
+
+route.get("/:thing", function(req, res){
+	res.send("Hello "  + req.params.thing);
+});
+
+module.exports = route;
