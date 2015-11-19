df862444fa4bf9d718a9686d322fdeda76cce39b
xDinomode <xDinomode@users.noreply.github.com>
2015-11-18 19:35:58 -0600

    added github link at the bottom of page. Added links to learn NodeJS

--- routes/mainrouter.js
+++ routes/mainrouter.js
@@ -2,6 +2,7 @@ var express = require("express");
 var route = express.Router();
 
 route.get("/", function(req, res){
+            res.set("X-hey", "testing");
 	res.render("index", {title: "Home Page", message: "global message"});
 });
 
